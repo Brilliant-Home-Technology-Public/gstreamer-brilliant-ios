8d54507f4997309ae1bb497b2492846aa5fc1ee2
Kiyoshi Shikuma <kiyoshi@brilliant.tech>
2022-05-11 16:36:36 -0700

    Add LGPL License header to all files and to root directory.

--- GStreamer-Brilliant/GStreamer-Brilliant/GStreamerRTSPBackend.m
+++ GStreamer-Brilliant/GStreamer-Brilliant/GStreamerRTSPBackend.m
@@ -1,3 +1,25 @@
+/*****************************************************************************
+ * GStreamer-Brilliant: Dynamic XCFramework built with system's GStreamer Implementation. Intended for use in Brilliant Mobile App.
+ *****************************************************************************
+ * Copyright (C) 2022 Brilliant Home Technologies
+ *
+ * Authors: Brilliant iOS Team <apple_developer # brilliant.tech>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdocumentation"
 #pragma clang diagnostic ignored "-Wunused-variable"
